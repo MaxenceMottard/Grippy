1f95d9a7db3ad0fd38b74d1a17ca482750beb595
maxencemottard <mottard.maxence@gmail.com>
2021-06-16 00:01:30 +0200

    Initial Commit

---
+++ FileOptimiser/FileOptimiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileOptimiserApp.swift
+//  FileOptimiser
+//
+//  Created by Maxence Mottard on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FileOptimiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
